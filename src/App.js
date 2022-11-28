0217fd0a8519e974e0efde06ecce7048c3f68ea6
Fpitkat <75321987+Fpitkat@users.noreply.github.com>
2022-11-28 13:12:51 -0500

    buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import Button from './Button'
+
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <div>
+        <Button
+          primary
+          outline>
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Hide ads</Button>
+      </div>
+      <div>
+        <Button warning>Buy now</Button>
+      </div>
+      <div>
+        <Button
+          danger
+          rounded>
+          Delete
+        </Button>
+        <div>
+          <Button success>Delete</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
